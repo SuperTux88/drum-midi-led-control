a6d207fd1c99e05f20879cc37e0814a731dc849c
Benjamin Neff <benjamin@coding4coffee.ch>
2022-01-10 02:28:32 +0100

    Add drum note values

---
+++ src/midi/DrumNotes.h
@@ -0,0 +1,33 @@
+#ifndef DRUM_MIDI_LED_CONTROL_DRUMNOTES_H
+#define DRUM_MIDI_LED_CONTROL_DRUMNOTES_H
+
+#define KICK 36
+
+#define SNARE 38
+#define SNARE_XSTICK 37
+#define SNARE_RIM 40
+#define SNARE_CONTROL 16
+
+#define TOM1 48
+#define TOM1_RIM 50
+#define TOM2 45
+#define TOM2_RIM 47
+#define TOM3 43
+#define TOM3_RIM 58
+
+#define HIHAT 46
+#define HIHAT_EDGE 26
+#define HIHAT_FOOT 44
+#define HIHAT_CONTROL 4
+
+#define RIDE 51
+#define RIDE_EDGE 59
+#define RIDE_BELL 53
+#define RIDE_CONTROL 16
+
+#define CRASH1 49
+#define CRASH1_EDGE 55
+#define CRASH2 57
+#define CRASH2_EDGE 52
+
+#endif //DRUM_MIDI_LED_CONTROL_DRUMNOTES_H
